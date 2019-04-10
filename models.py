cfadfd09958c8963508317a41f29b0d055d93eeb
sujinunt <sujinunt.p@ku.th>
2019-04-10 22:28:31 +0700

    Add Zombie Group

--- models.py
+++ models.py
@@ -1,10 +1,33 @@
 import arcade.key
+import math
+import random
 MOVEMENT_SPEED = 5
+Zombie_SPEED = 0.7
+
 class Model:
     def __init__(self, world, x, y):
         self.world = world
         self.x = x
         self.y = y
+
+class Zombie(arcade.Sprite):
+
+    def follow_sprite(self, player_sprite):
+        self.center_x += self.change_x
+        self.center_y += self.change_y
+        if random.randrange(100) == 0:
+            start_x = self.center_x
+            start_y = self.center_y
+            dest_x = player_sprite.center_x
+            dest_y = player_sprite.center_y
+
+            x_diff = dest_x - start_x
+            y_diff = dest_y - start_y
+            angle = math.atan2(y_diff, x_diff)
+
+            self.change_x = math.cos(angle) * Zombie_SPEED
+            self.change_y = math.sin(angle) * Zombie_SPEED
+
 
 
 class Surviver(Model):
@@ -15,26 +38,44 @@ class Surviver(Model):
 
     def update(self, delta):
         self.x += self.vx
-        if self.x >= self.world.width:
-            self.x = self.world.width
+        self.y +=self.vy
+        if self.x >= self.world.width-18:
+            self.x = self.world.width-18
+        elif self.x <= 22:
+            self.x = 22
+        elif self.y >= self.world.height-50:
+            self.y = self.world.height-50
+        elif self.y <= 50:
+            self.y = 50
 
 
 class World:
     def __init__(self, width, height):
         self.width = width
         self.height = height
-
+        self.weapon = 0 #no weapon
         self.surviver = Surviver(self, 50, 100)
 
     def update(self, delta):
         self.surviver.update(delta)
+
 
     def on_key_press(self, key, key_modifiers):
         if key == arcade.key.A:
             self.surviver.vx-=MOVEMENT_SPEED
         elif key == arcade.key.D:
             self.surviver.vx+=MOVEMENT_SPEED
+        elif key == arcade.key.W:
+            self.surviver.vy+=MOVEMENT_SPEED
+        elif key == arcade.key.S:
+            self.surviver.vy-=MOVEMENT_SPEED
+        elif key == arcade.key.F:
+            self.weapon = 1
+        elif key == arcade.key.E:
+            self.weapon=0
 
     def on_key_release(self, key, modifiers):
         if key == arcade.key.A or key == arcade.key.D:
             self.surviver.vx = 0
+        elif key == arcade.key.W or key == arcade.key.S:
+            self.surviver.vy = 0
--- survivalnow.py
+++ survivalnow.py
@@ -1,16 +1,18 @@
 import arcade
-
+import random
 from models import World
+from models import Zombie
 
 SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 400
 SCREEN_TITLE = "Survival now"
+Zombie_count = 10
+
 
 
 class ModelSprite(arcade.Sprite):
     def __init__(self, *args, **kwargs):
         self.model = kwargs.pop('model', None)
-
         super().__init__(*args, **kwargs)
 
     def sync_with_model(self):
@@ -25,24 +27,43 @@ class ModelSprite(arcade.Sprite):
 class SurvivalnowWindow(arcade.Window):
     def __init__(self, width, height,title):
         super().__init__(width, height,title)
-
         self.world = World(SCREEN_WIDTH, SCREEN_HEIGHT)
         self.survival_sprite = ModelSprite('images/surviver_stay.png',
                                       model=self.world.surviver)
+        self.survival_spritegun = ModelSprite('images/surviver_gun.png',
+                                               model=self.world.surviver)
         self.total_time = 0.0
+        self.zombielist=None
+
 
     def setup(self):
-        arcade.set_background_color(arcade.color.GRAY)
+        self.background = arcade.load_texture("images/background.png")
         """Time set sec"""
         self.total_time = 1000.0
+        self.zombielist = arcade.SpriteList()
+        for i in range(Zombie_count):
+            zombie = Zombie("images/Zombie_stay.png", 1)
+            # Position the zombie
+            zombie.center_x = random.randint(600,700)
+            zombie.center_y = random.randrange(SCREEN_HEIGHT)
+            # Add the zombie to the lists
+            self.zombielist.append(zombie)
 
     def update(self, delta):
         self.world.update(delta)
         self.total_time -= delta
+        for zombie in self.zombielist:
+            zombie.follow_sprite(self.survival_sprite)
 
     def on_draw(self):
         arcade.start_render()
-        self.survival_sprite.draw()
+        arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2,
+                                      SCREEN_WIDTH, SCREEN_HEIGHT, self.background)
+        if self.world.weapon == 0:
+            self.survival_sprite.draw()
+        elif self.world.weapon == 1:
+            self.survival_spritegun.draw()
+        self.zombielist.draw()
         minutes = int(self.total_time) // 60
         seconds = int(self.total_time) % 60
         output = f"Time: {minutes:02d}:{seconds:02d}"
