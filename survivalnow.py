f4e64426991facec7b7b567e3d92b95441bd35d4
sujinunt <sujinunt.p@ku.th>
2019-04-10 22:53:59 +0700

    Add Bullet can shot zombie

--- survivalnow.py
+++ survivalnow.py
@@ -1,5 +1,6 @@
 import arcade
 import random
+import math
 from models import World
 from models import Zombie
 
@@ -7,7 +8,7 @@ SCREEN_WIDTH = 600
 SCREEN_HEIGHT = 400
 SCREEN_TITLE = "Survival now"
 Zombie_count = 10
-
+BULLET_SPEED = 5
 
 
 class ModelSprite(arcade.Sprite):
@@ -34,13 +35,14 @@ class SurvivalnowWindow(arcade.Window):
                                                model=self.world.surviver)
         self.total_time = 0.0
         self.zombielist=None
-
+        self.bullet_list = None
 
     def setup(self):
         self.background = arcade.load_texture("images/background.png")
         """Time set sec"""
         self.total_time = 1000.0
         self.zombielist = arcade.SpriteList()
+        self.bullet_list = arcade.SpriteList()
         for i in range(Zombie_count):
             zombie = Zombie("images/Zombie_stay.png", 1)
             # Position the zombie
@@ -54,6 +56,19 @@ class SurvivalnowWindow(arcade.Window):
         self.total_time -= delta
         for zombie in self.zombielist:
             zombie.follow_sprite(self.survival_sprite)
+        self.bullet_list.update()
+        for bullet in self.bullet_list:
+            # Check this bullet to see if it hit a coin
+            hit_list = arcade.check_for_collision_with_list(bullet, self.zombielist)
+            # If it did, get rid of the bullet
+            if len(hit_list) > 0:
+                bullet.kill()
+            # For every coin we hit, add to the score and remove the coin
+            for zombie in hit_list:
+                zombie.kill()
+            # If the bullet flies off-screen, remove it.
+            if bullet.bottom > self.width or bullet.top < 0 or bullet.right < 0 or bullet.left > self.width:
+                bullet.kill()
 
     def on_draw(self):
         arcade.start_render()
@@ -64,6 +79,7 @@ class SurvivalnowWindow(arcade.Window):
         elif self.world.weapon == 1:
             self.survival_spritegun.draw()
         self.zombielist.draw()
+        self.bullet_list.draw()
         minutes = int(self.total_time) // 60
         seconds = int(self.total_time) % 60
         output = f"Time: {minutes:02d}:{seconds:02d}"
@@ -74,6 +90,23 @@ class SurvivalnowWindow(arcade.Window):
 
     def on_key_release(self, key, key_modifiers):
         self.world.on_key_release(key,key_modifiers)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        # Create a bullet
+        bullet = arcade.Sprite("images/Bullet.png", 0.8)
+        start_x = self.survival_sprite.center_x
+        start_y = self.survival_sprite.center_y
+        bullet.center_x = start_x
+        bullet.center_y = start_y
+        dest_x = x
+        dest_y = y
+        x_diff = dest_x - start_x
+        y_diff = dest_y - start_y
+        angle = math.atan2(y_diff, x_diff)
+        bullet.angle = math.degrees(angle)
+        bullet.change_x = math.cos(angle) * BULLET_SPEED
+        bullet.change_y = math.sin(angle) * BULLET_SPEED
+        self.bullet_list.append(bullet)
 
 
 if __name__ == '__main__':
